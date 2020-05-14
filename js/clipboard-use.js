0e82f44b5165a95006602701979bbaa8769b7833
sjsmi1e <1043110319@qq.com>
2020-05-14 13:15:56 +0800

    Site updated: 2020-05-14 13:15:46

--- js/clipboard-use.js
+++ js/clipboard-use.js
@@ -4,21 +4,27 @@
 	var initCopyCode = function(){
 
 		var copyHtml = '';
-		copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+		copyHtml += '<button id="copyBtn" class="btn-copy" data-clipboard-snippet="">';
 		copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
 		copyHtml += '</button>';
+		// copyHtml += '<script type="text/javascript" src="/js/alert.js"></script>';
 		$(".highlight .code pre").before(copyHtml);
 		var clipboard = new ClipboardJS('.btn-copy', {
 			target: function(trigger) {
 				return trigger.nextElementSibling;
 			}
 		});
-	//复制成功事件绑定
-	clipboard.on('success',
-		function(e) {
+		// //复制成功事件绑定
+    	clipboard.on('success',
+    		function(e) {
                 //清除内容被选择状态
                 e.clearSelection();
                 // changeToSuccess(e);
+                jqueryAlert({
+                	'icon'    : '/img/right.png',
+                	'content' : '复制成功',
+                	'closeTime' : 2000,
+                })
             });
         //复制失败绑定事件
         clipboard.on('error',
@@ -26,6 +32,7 @@
         		console.error('Action:', e.action);
         		console.error('Trigger:', e.trigger);
         	});
-    }
-    initCopyCode();
-}(window, document);
+	}
+	initCopyCode();
+    	
+    }(window, document);
