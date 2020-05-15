ed28eac1d39023144963637a4519495af08d41d9
sjsmi1e <1043110319@qq.com>
2020-05-15 18:01:45 +0800

    Site updated: 2020-05-15 18:01:40

--- js/music.js
+++ js/music.js
@@ -2,13 +2,13 @@
 * @Author: smile
 * @Date:   2020-05-15 10:28:50
 * @Last Modified by:   smile
-* @Last Modified time: 2020-05-15 16:47:09
+* @Last Modified time: 2020-05-15 17:59:01
 */
 $(function(){
 	if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
 		console.log('手机');
 		$('#main').append('<meting-js\n'+
-        'style="position:fixed;left:20px;bottom:20px;"\n'+
+        'style="position:fixed;left:20px;bottom:20px;z-index=1"\n'+
         'server="netease"\n'+
         'type="playlist"\n'+
         'id="2297228045"\n'+
@@ -28,4 +28,7 @@ $(function(){
         'list-folded="true">')
 	}
 	$('#main').append('</meting-js>');
+	$("meting-js").draggable({ 
+	    	containment: "#main"
+	});
 })
