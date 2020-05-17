58722f1861d780af9f7b0d36a8b6fbf9b7c13e12
sjsmi1e <1043110319@qq.com>
2020-05-17 13:00:10 +0800

    Site updated: 2020-05-17 13:00:08

--- js/music.js
+++ js/music.js
@@ -2,16 +2,16 @@
 * @Author: smile
 * @Date:   2020-05-15 10:28:50
 * @Last Modified by:   smile
-* @Last Modified time: 2020-05-15 17:59:01
+* @Last Modified time: 2020-05-17 12:53:40
 */
 $(function(){
 	if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
 		console.log('手机');
 		$('#main').append('<meting-js\n'+
         'style="position:fixed;left:20px;bottom:20px;z-index=1"\n'+
-        'server="netease"\n'+
+        'server="tencent"\n'+
         'type="playlist"\n'+
-        'id="2297228045"\n'+
+        'id="3543342280"\n'+
         'autoplay="true"\n'+
         'order="random"\n'+
         'mini="true">')
@@ -20,9 +20,9 @@ $(function(){
 		console.log('电脑');
 		$('#main').append('<meting-js\n'+
         'style="position:fixed;left:20px;bottom:20px;"\n'+
-        'server="netease"\n'+
+        'server="tencent"\n'+
         'type="playlist"\n'+
-        'id="2297228045"\n'+
+        'id="3543342280"\n'+
         'autoplay="true"\n'+
         'order="random"\n'+
         'list-folded="true">')
